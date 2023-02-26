08cc996f33bb239edb26ae4f7b351a68f64bd09b
Shameera Anuranga <shameera.a@synergenhealth.com>
2023-02-26 21:00:00 +0530

    new test case added

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,26 @@
 package com.example.demo;
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest class DemoApplicationTests {
+import static org.hamcrest.CoreMatchers.equalTo;
 
-	@Test void contextLoads() {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests {
+
+	@Test
+	void demoTest() {
+		RestAssured.given().contentType(ContentType.JSON)
+				.get("https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false").then()
+				.statusCode(HttpStatus.SC_OK)
+				.body("Name",equalTo("Carbon credits"))
+				.body("CanRelist",equalTo(true))
+				.body("Promotions.find{it.Name == 'Gallery'}.Description",equalTo("Good position in category"));
 	}
-
 }
